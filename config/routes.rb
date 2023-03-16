5eb2fad98fea9c85a775955bf1a5baccb789bd09
mohamed_ahmed_sleem <aliahmed2951123@gmail.com>
2023-03-16 02:00:38 +0200

    section 6 associations and authentication systems

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
-   root 'pages#home'
-   get 'about', to: 'pages#about'
+
   # get 'test', to: 'articles#test'
   # resources :articles , only: [ :index, :new, :create,:update,:edit,:test]# articles/1
   #  delete 'articles/:id' , to: 'articles#destroy'
   # delete 'articles/:id/delete' => 'articles#destroy', as: 'articles_delete'
   # get '/articles/:id/delete' => 'articles#destroy'
+   root 'pages#home'
+   get 'about', to: 'pages#about'
   resources :articles
+
 =begin
   REST - Representational state transfer - mapping HTTP verbs (get, post, put/patch, delete)  
   to CRUD actions 
